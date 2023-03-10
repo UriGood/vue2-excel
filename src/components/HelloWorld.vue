1487f4042fd01d7e6fdd3016e7dd6886808ac01b
UriGood <urielmf.ing@gmail.com>
2023-03-09 21:11:27 -0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-data-table :headers="headers" class="elevation-1" 
+        :items="desserts"
+        ></v-data-table>
+      </v-col>
+      <!-- :items="desserts"  -->
+      <!-- :items-per-page="5"  -->
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'User ID',
+          align: 'start',
+          sortable: false,
+          value: '_id',
+        },
+        { text: 'User Name', value: 'userName' },
+        { text: 'Date', value: 'date' },
+        { text: 'Punch In', value: 'punchIn' },
+        { text: 'Punch Out', value: 'punchOut' },
+
+      ],
+      desserts: [],
+    }
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get("http://18.224.73.103/api/users")
+        .then((result) => {
+          this.desserts = result.data.data;
+        })
+
+    }
+  },
+  created() {
+    this.getUsers();
+  }
+}
+</script>
